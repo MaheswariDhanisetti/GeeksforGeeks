722259611e5917b7e3a6a449bee5b60f84082526
Maheswari Dhanisetti <119394667+MaheswariDhanisetti@users.noreply.github.com>
2024-05-27 22:57:15 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Trailing zeroes in factorial/trailing-zeroes-in-factorial.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    int trailingZeroes(int number)
+    {
+        // Write Your Code here
+       int count = 0;
+       for (int i = 5; number / i >= 1; i *= 5){
+          int temp = number / i;
+          count = count + temp;
+       }
+       return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        int ans  = ob.trailingZeroes(N);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
