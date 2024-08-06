92fc714b4c31225380e7420ca556f1bb1c7ec6fe
Maheswari Dhanisetti <maheswarimahi2005@gmail.com>
2024-08-06 22:43:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Validate an IP Address/validate-an-ip-address.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class validip {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            String s = sc.next();
+            Solution obj = new Solution();
+
+            if (obj.isValid(s))
+                System.out.println("true");
+            else
+                System.out.println("false");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+
+    public boolean isValid(String str) {
+        // Write your code here
+          String PATTERN = "^((0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)\\.){3}(0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)$";
+    return str.matches(PATTERN); 
+    }
+}
