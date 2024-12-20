de66c1439f5561c2e8aa225a57e472e988025b6a
Maheswari Dhanisetti <maheswarimahi2005@gmail.com>
2024-12-20 09:42:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -8,17 +8,44 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
+  
+    int binary_search ( vector<int>&nums , int target , int dir){
+
+        int low = 0 , high = nums.size()-1;
+        int ans = -1;
+        while(low<=high){
+
+            int mid = (low+high)/2;
+
+            if(nums[mid]==target){
+                ans = mid;
+                
+                if(dir==0){
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            else if(nums[mid]<target) low = mid+1;
+            else high = mid-1;
+        }
+
+        return ans;
+
+    }
+    
     int countFreq(vector<int>& arr, int target) {
         // code here
         
-        int cnt=0;
         
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]==target){
-                cnt++;
-            }
-        }
-        return cnt;
+        int fir = binary_search(arr, target , 0);
+        int sec = binary_search(arr, target , 1);
+
+        if(fir==-1 and sec==-1) return 0;
+        
+        return sec-fir+1;
+        
     }
 };
 
